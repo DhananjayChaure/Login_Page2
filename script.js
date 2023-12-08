0a9904161ca537151263ae5bef9401fd57d4c0bb
Dhananjay <111357724+DhananjayChaure@users.noreply.github.com>
2023-12-08 11:43:20 +0530

    Uploaded script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+//Validation Code for Inputs
+
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+    //email:
+    if (email.value == '') {
+        document.getElementById("email_error").innerHTML="Please fill up your Email or Phone";
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if (email.value.length < 9) {
+        document.getElementById("email_error").innerHTML="Email length is less";
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if (email.value!='abcd@gmail.com') {
+        document.getElementById("email_error").innerHTML="Wrong email...!";
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+
+    //password:
+    if (password.value == '') {
+        document.getElementById("pass_error").innerHTML="Please fill up your Password";
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+    if (password.value.length < 6) {
+        document.getElementById("pass_error").innerHTML="Password length is less";
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+    if (password.value != 'abcd19') {
+        document.getElementById("pass_error").innerHTML="Wrong Password...!";
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+}
+
+
+
+function email_Verify(){
+    if (email.value.length >= 8) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+    if (email.value == 'abcd@gmail.com') {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+function pass_Verify(){
+    if (password.value.length >= 5) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+    if (password.value == 'abcd19') {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+}
